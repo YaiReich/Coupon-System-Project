f6d994ed0c9ece7f1acfa5b6b6d4541663c72f9b
YaiReich <117544089+YaiReich@users.noreply.github.com>
2023-04-19 22:08:04 +0300

    Update App.js

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -7,7 +7,15 @@ import NotificationContainer from "react-notifications/lib/NotificationContainer
 const App = () => {
 
     const token = useSelector(state => state.auth.token)
-
+ 
+    return (
+        <div className="App">
+            <NotificationContainer />
+            <Routes>
+                <Route path="/" element={token ? <Home /> : <Login />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
